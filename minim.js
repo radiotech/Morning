1fda393832b5e6e09e8c91cafa69e5ffa2cc40e4
A.J. <lavabirdaj@gmail.com>
2015-01-09 07:16:02 -0500

    ogg true

--- minim.js
+++ minim.js
@@ -25,7 +25,7 @@ function AudioPlayer(str) {
     audio.preload = 'auto';
     audio.autobuffer = true;
     if (canPlayOgg()) {
-      audio.src = str;//audio.src = str.split(".")[0] + ".ogg";
+      audio.src = str.split(".")[0] + ".ogg";
     } else if (canPlayMp3()) {
       audio.src = str;
     }
